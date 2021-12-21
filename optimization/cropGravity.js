56bf0d4f14cda3a0c8e7050ac6286340e4eaaaaa
rebeccapeltz <rebecca.peltz@cloudinary.com>
2021-12-20 16:43:16 -0800

    add cropGravity

---
+++ optimization/cropGravity.js
@@ -0,0 +1,9 @@
+// crop using the c_, crop option
+// use gravity, g_ to spotlight a portion of the image
+// https://cloudinary.com/documentation/resizing_and_cropping
+
+// fill	    Resizes the image to fill the specified dimensions without distortion. The image may be cropped as a result.
+// lfill	Same as fill, but only scales down the image.
+// fill_pad	Same as fill, but avoids excessive cropping by adding padding when needed. Supported only with automatic cropping.
+// crop	    Extracts a region of the specified dimensions from the original image without first resizing it.
+// thumb	Creates a thumbnail of the image with the specified dimensions, based on a specified gravity. Scaling may occur.
