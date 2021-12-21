91085a6add64ba527441d94a58c0d6c8715de06f
rebeccapeltz <rebecca.peltz@cloudinary.com>
2021-12-20 21:16:38 -0800

    add format

---
+++ optimization/format.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const cloudinary = require('cloudinary').v2
+cloudinary.config({ secure: 'true' })
+
+// with resizing +- fetchFormat
+console.log('----resizing/gravity/format')
+console.log(cloudinary.url('doctor', { crop: 'scale', width: 400, format: 'jpg' }))
+console.log(cloudinary.url('doctor', { crop: 'scale', width: 400, fetch_format: 'auto' }))
+
+// with cropping/gravity +- fetch_format
+console.log('----cropping/gravity/format')
+console.log(cloudinary.url('doctor', { crop: 'thumb', gravity: 'face', width: 400, height: 400, format: 'jpg' }))
+console.log(cloudinary.url('doctor', { crop: 'thumb', gravity: 'face', width: 400, height: 400, fetch_format: 'auto' }))
+
+// with resizing f_auto +- q_auto
+console.log('----resizing/gravity/format/quality')
+console.log(cloudinary.url('doctor', { crop: 'scale', width: 400, fetch_format: 'auto' }))
+console.log(cloudinary.url('doctor', { crop: 'scale', width: 400, quality:'auto', fetch_format: 'auto' }))
+
+// with cropping f_auto +- q_auto
+console.log('----cropping/gravity/format/quality')
+console.log(cloudinary.url('doctor', { crop: 'thumb', gravity: 'face', width: 400, height: 400, fetch_format: 'auto' }))
+console.log(cloudinary.url('doctor', { crop: 'thumb', gravity: 'face', width: 400, height: 400, quality:'auto', fetch_format: 'auto' }))
