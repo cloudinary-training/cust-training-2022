fa3c57c4b075ed4d670ee931cf5af9cda7634d78
rebeccapeltz <rebecca.peltz@cloudinary.com>
2021-12-28 10:42:34 -0800

    compare fit and thumb

--- optimization/images/format.js
+++ optimization/images/format.js
@@ -2,14 +2,10 @@ require("dotenv").config();
 const cloudinary = require("cloudinary").v2;
 cloudinary.config({ secure: "true" });
 
-// with resizing +- fetchFormat
-console.log("----resizing/gravity/format");
-console.log(
-  cloudinary.url("doctor", { crop: "scale", width: 400, format: "jpg" })
-);
-console.log(
-  cloudinary.url("doctor", { crop: "scale", width: 400, fetch_format: "auto" })
-);
+// focus on resize(fit) and crop (thumb)
+// best practice recommendation when working with f_auto: chain it and if you're using q_auto, you can chain them together
+
+
 
 // with cropping/gravity +- fetch_format
 console.log("----cropping/gravity/format");
@@ -22,53 +18,7 @@ console.log(
     format: "jpg",
   })
 );
-console.log(
-  cloudinary.url("doctor", {
-    crop: "thumb",
-    gravity: "face",
-    width: 400,
-    height: 400,
-    fetch_format: "auto",
-  })
-);
 
-// with resizing f_auto +- q_auto
-console.log("----resizing/gravity/format/quality");
-console.log(
-  cloudinary.url("doctor", { crop: "scale", width: 400, fetch_format: "auto" })
-);
-console.log(
-  cloudinary.url("doctor", {
-    crop: "scale",
-    width: 400,
-    quality: "auto",
-    fetch_format: "auto",
-  })
-);
-
-// with cropping f_auto +- q_auto
-console.log("----cropping/gravity/format/quality");
-console.log(
-  cloudinary.url("doctor", {
-    crop: "thumb",
-    gravity: "face",
-    width: 400,
-    height: 400,
-    fetch_format: "auto",
-  })
-);
-console.log(
-  cloudinary.url("doctor", {
-    crop: "thumb",
-    gravity: "face",
-    width: 400,
-    height: 400,
-    quality: "auto",
-    fetch_format: "auto",
-  })
-);
-
-// best practice recommendation when working with f_auto: chain it and if you're using q_auto, you can chain them together
 console.log(
   cloudinary.url("doctor", {
     transformation: [
@@ -77,9 +27,45 @@ console.log(
         gravity: "face",
         width: 400,
         height: 400,
-        quality: "auto",
-        fetch_format: "auto",
       },
+      { fetch_format: "auto" },
+    ],
+  })
+);
+
+console.log(
+  cloudinary.url("doctor", {
+    transformation: [
+      {
+        crop: "thumb",
+        gravity: "face",
+        width: 400,
+        height: 400,
+      },
+      { quality: "auto" },
+      { fetch_format: "auto" },
+    ],
+  })
+);
+
+// with resizing f_auto +- q_auto
+console.log("----resizing/gravity/format/quality");
+console.log(
+  cloudinary.url("doctor", {
+    transformation: [{ crop: "fit", width: 400,height:400, format: "jpg" }],
+  })
+);
+console.log(
+  cloudinary.url("doctor", {
+    transformation: [{ crop: "fit", width: 400,height:400 }, { fetch_format: "auto" }],
+  })
+);
+console.log(
+  cloudinary.url("doctor", {
+    transformation: [
+      { crop: "fit", width: 400,height:400 },
+      { quality: "auto" },
+      { fetch_format: "auto" }
     ],
   })
 );
