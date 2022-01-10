ef31b3fa24e5ef44cbe8ea416270e102c9fa7b90
rebeccapeltz <rebecca.peltz@cloudinary.com>
2022-01-10 11:07:25 -0800

    fix video script to match html page

--- optimization/video/qualityFormat.js
+++ optimization/video/qualityFormat.js
@@ -14,19 +14,55 @@ console.log("-----with quality and format optimization");
 console.log(
   cloudinary.url("hat", {
     resource_type: "video",
-    format: "mp4",
     transformation: [{ quality: "auto" }, { fetch_format: "auto" }],
   })
 );
+
+
 
 console.log("-----with resize, quality and format optimization");
 console.log(
   cloudinary.url("hat", {
     resource_type: "video",
-    format: "mp4",
     transformation: [
-      { crop: "fit", height: "400", width: "400" },
+      { crop: "fit", height: "500", width: "500"},
       { quality: "auto" },
+      { fetch_format: "auto" },
+    ],
+  })
+);
+
+console.log("-----with resize, quality auto and format auto optimization");
+console.log(
+  cloudinary.url("hat", {
+    resource_type: "video",
+    transformation: [
+      { crop: "fit", height: "500", width: "500"},
+      { quality: "auto" },
+      { fetch_format: "auto" },
+    ],
+  })
+);
+
+console.log("-----with resize, quality 70 and format auto optimization");
+console.log(
+  cloudinary.url("hat", {
+    resource_type: "video",
+    transformation: [
+      { crop: "fit", height: "500", width: "500"},
+      { quality: "70" },
+      { fetch_format: "auto" },
+    ],
+  })
+);
+
+console.log("-----with resize, quality 80 and format auto optimization");
+console.log(
+  cloudinary.url("hat", {
+    resource_type: "video",
+    transformation: [
+      { crop: "fit", height: "500", width: "500"},
+      { quality: "80" },
       { fetch_format: "auto" },
     ],
   })
