d78ff994f9da2be266cd924d102460ded179b57a
rebeccapeltz <rebecca.peltz@cloudinary.com>
2022-01-27 11:51:56 -0800

    cleanp apos

--- access-control/authenticated/authenticated-with-optimization.js
+++ access-control/authenticated/authenticated-with-optimization.js
@@ -1,18 +1,18 @@
-require("dotenv").config();
-const cloudinary = require("cloudinary").v2;
+require('dotenv').config();
+const cloudinary = require('cloudinary').v2;
 
 // authenticated type: no access to original unless URL is signed
 // sign_url signs the image and transformations
 // secure: true make the URL HTTPS
 // add f_auto, q_auto 
 
-const url = cloudinary.url("dolphin", {
-  type: "authenticated",
+const url = cloudinary.url('dolphin', {
+  type: 'authenticated',
   secure: true,
   sign_url: true,
   transformation: [
-    { width: 300, height: 300, crop: "limit" },
-    { fetch_format: "auto", quality: "auto" },
+    { width: 300, height: 300, crop: 'limit' },
+    { fetch_format: 'auto', quality: 'auto' },
   ],
 });
 console.log(url);
