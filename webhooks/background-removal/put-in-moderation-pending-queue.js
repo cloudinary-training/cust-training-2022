fa39ae16de9bdb03a65d061911e5d49b01b1c375
rebeccapeltz <rebecca.peltz@cloudinary.com>
2022-03-04 17:11:11 -0800

    explicit recognize notification_url?

--- webhooks/background-removal/put-in-moderation-pending-queue.js
+++ webhooks/background-removal/put-in-moderation-pending-queue.js
@@ -5,12 +5,9 @@ const cloudinary = require('cloudinary').v2;
 // put the image in the "manual" moderation queue
 // you need to manually approve in the DAM manual moderation queue
 cloudinary.uploader
-  .explicit('yellow-shoes1', {
+  .explicit('yellow-shoes-02', {
     type: 'authenticated',
-    moderation: 'manual',
-    notification_url:
-      'https://webhook.site/1a0678f1-afc3-4077-8666-e232a5fe8c2d',
-  })
+    moderation: 'manual'
   .then((result) => {
     console.log(JSON.stringify(result, null, 2));
   })
