e0f37c2099ea32fc585f351dccbc4ec302796ed0
rebeccapeltz <rebecca.peltz@cloudinary.com>
2022-06-02 15:09:08 -0700

    add spinning cadillac

--- product-gallery/rich-media-examples/upload-3d-model.js
+++ product-gallery/rich-media-examples/upload-3d-model.js
@@ -1,18 +1,30 @@
 require('dotenv').config();
 const cloudinary = require('cloudinary').v2;
 
-// cloudinary.uploader
-//   .upload('./assets/CLdLogo3D.glb', {
-//     public_id: 'cld-logo-3d',
-//     resource_type: 'image',
-//   })
-//   .then((result) => {
-//     console.log(result.secure_url);
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
+cloudinary.uploader
+  .upload('./assets/CLdLogo3D.glb', {
+    public_id: 'cld-logo-3d',
+    resource_type: 'image',
+  })
+  .then((result) => {
+    console.log(result.secure_url);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
+
+  cloudinary.uploader
+  .upload('./assets/Cadillac_75_Sedan_1953.glb', {
+    public_id: 'cadillac',
+    resource_type: 'image',
+  })
+  .then((result) => {
+    console.log(result.secure_url);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
   cloudinary.uploader
   .upload('./assets/Cadillac_75_Sedan_1953.glb', {
