0d75ad83648729696805712786b150b7825917ce
rebeccapeltz <rebecca.peltz@cloudinary.com>
2022-07-08 08:39:47 -0700

    cleanup aggregation

--- admin-search/aggregate.js
+++ admin-search/aggregate.js
@@ -2,18 +2,8 @@ require('dotenv').config();
 const cloudinary = require('cloudinary').v2;
 console.log(cloudinary.config().cloud_name);
 
-// cloudinary.search.execute({aggregate:'image',}).then(result=>console.log(result));
-
-// cloudinary.search
-//   //   .aggregate('resource_type')
-//   .expression('uploaded_at>30d')
-//   .aggregate('format')
-//   .max_results(5)
-//   .execute()
-//   .then((result) => console.log(result));
-
 cloudinary.search
-  .expression('uploaded_at>30d AND resource_type:video')
+  .expression('uploaded_at<30d AND resource_type:video')
   .aggregate('format')
   .max_results(5)
   .execute()
