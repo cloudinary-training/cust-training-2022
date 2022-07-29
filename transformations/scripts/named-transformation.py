3110cb4207b6daa08fcb0051e4eee3136cd5ae9f
jenbrissman <brissman514@gmail.com>
2022-07-29 16:43:51 -0400

    named transformations

---
+++ transformations/scripts/named-transformation.py
@@ -0,0 +1,20 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import json
+import cloudinary.api
+import cloudinary.uploader
+import cloudinary
+
+
+config = cloudinary.config(secure=False)
+
+#creating named transformation
+cloudinary.api.create_transformation('example_NT', dict(
+    transformation=[{'crop': 'scale', 'width': 200, 'gravity': 'face'}]))
+
+#applying named transformation to a file
+print(cloudinary.url("_", { transformation: ["example_NT"] }))
+
+#deleting named transformation
+# cloudinary.api.delete_transformation('example_NT')
