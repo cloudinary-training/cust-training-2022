aa132f91fe993f496b7559c0f600485a3ad85c35
rebeccapeltz <rebecca.peltz@cloudinary.com>
2022-08-04 13:51:59 -0700

    add upload example script

--- transformations/Python/uploadExampleAssets.py
+++ transformations/Python/uploadExampleAssets.py
@@ -15,23 +15,24 @@ def uploadAsset(source,public_id,resource_type):
 
     resp = cloudinary.uploader.upload(source, public_id=public_id, resource_type=resource_type, overwrite=True)
     print(json.dumps(resp, indent=4, sort_keys=True))
-  
-
-    # cloudinary.uploader.upload("https://res.cloudinary.com/cloudinary-training/video/upload/wave.mp4",
-    #                            public_id="wave",resource_type="video", overwrite=True)
-
-    # cloudinary.uploader.upload("https://res.cloudinary.com/cloudinary-training/video/upload/images/forest-reflection",
-    #                            public_id="wave", overwrite=True)
-    
-    # cloudinary.uploader.upload("https://res.cloudinary.com/cloudinary-training/video/upload/images/green-car.jpg",
-    #                            public_id="green-car", overwrite=True)
 
 def main():
     validateConfig()
+    # frontend
     uploadAsset("https://res.cloudinary.com/jen-brissman/image/upload/butterfly.jpg","butterfly","image")
     uploadAsset("https://res.cloudinary.com/cloudinary-training/video/upload/wave.mp4", "wave", "video")
-    uploadAsset("https://res.cloudinary.com/cloudinary-training/image/upload/images/forest-reflection", "images/forest-reflection", "image")
+    uploadAsset("https://res.cloudinary.com/cloudinary-training/image/upload/images/forest-reflection", "cld-forest-reflection", "image")
     uploadAsset("https://res.cloudinary.com/cloudinary-training/image/upload/green-car.jpg","green-car","image")
+
+    # backend
+    uploadAsset("./assets/chameleon.jpg","chameleon","image")
+    uploadAsset("./assets/closeupchameleon.mp4","chameleon","video")
+    # uploadAsset("./assets/gal.jpg","gal","image")
+    # CAN YOU USE THE BUILT IN cld-sample.jpg instead of uploading with a different public id?
+    uploadAsset("./assets/logo.png","cld-training-logo","image")
+    # can we rename logo to cld-training-logo?
+
+
    
 
 
