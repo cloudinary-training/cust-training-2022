265d0c842099d7a7b9f529c343b2128c567d1276
rebeccapeltz <rebecca.peltz@cloudinary.com>
2022-08-05 14:44:33 -0700

    add a script to create url with a named transformation

---
+++ transformations/Python/use_named_transformation.py
@@ -0,0 +1,25 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import cloudinary
+from cloudinary.utils import cloudinary_url
+
+config = cloudinary.config(secure=True)
+
+def validate_config():
+    print(cloudinary.config().cloud_name)
+
+
+def use_named_transformation():
+    url, options = cloudinary_url(
+        "doctor-profile",
+       transformation="avatar"
+    )
+    print("****Use a named transformation****\nTransformation URL --> " + url)
+
+def main():
+    validate_config()
+    use_named_transformation()
+
+
+main()
