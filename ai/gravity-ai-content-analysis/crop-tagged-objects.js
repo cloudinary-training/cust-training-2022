82e0f90b45801f6da1237a9016ad60eb71a40121
rebeccapeltz <rebecca.peltz@cloudinary.com>
2022-09-09 16:01:46 -0700

    add other object crops

--- ai/gravity-ai-content-analysis/crop-tagged-objects.js
+++ ai/gravity-ai-content-analysis/crop-tagged-objects.js
@@ -3,23 +3,154 @@ const cloudinary = require('cloudinary').v2;
 
 // example of crop with default gravity
 // this image original size is about 3000 x 6000
-console.log(cloudinary.url('fashion-athena',{crop:"crop",width:1500,height:3000}))
+console.log(
+  cloudinary.url('fashion-athena', { crop: 'crop', width: 400, height: 400 })
+);
 
+// gravity auto
+console.log(
+  cloudinary.url('fashion-athena', {
+    crop: 'crop',
+    gravity: 'auto',
+    width: 400,
+    height: 400,
+  })
+);
+// with and without gravity face
+console.log(
+  cloudinary.url('models', {
+    crop: 'thumb',
+    gravity: 'face',
+    width: 400,
+    height: 400,
+  })
+);
+console.log(
+  cloudinary.url('models', {
+    crop: 'thumb',
+    width: 400,
+    height: 400,
+  })
+);
+
+// cropping video with and without g_auto
+
+console.log(
+  cloudinary.url('dog-frisbee-video.mp4', {
+    resource_type: 'video',
+    transformation: [
+      { crop: 'fill', width: 400, height: 400 },
+      { fetch_format: 'auto' },
+      { quality: 'auto' },
+    ],
+  })
+);
+
+console.log(
+  cloudinary.url('dog-frisbee-video.mp4', {
+    resource_type: 'video',
+    transformation: [
+      { crop: 'fill', width: 400, height: 400, gravity: 'auto' },
+      { fetch_format: 'auto' },
+      { quality: 'auto' },
+    ],
+  })
+);
 
 // example of crop with compass gravity
-console.log(cloudinary.url('fashion-athena',{crop:"crop",width:1500,height:3000,gravity:"north_west"}))
-console.log(cloudinary.url('fashion-athena',{crop:"crop",width:1500,height:3000,gravity:"north_east"}))
-console.log(cloudinary.url('fashion-athena',{crop:"crop",width:1500,height:3000,gravity:"south_west"}))
-console.log(cloudinary.url('fashion-athena',{crop:"crop",width:1500,height:3000,gravity:"south_east"}))
+console.log(
+  cloudinary.url('fashion-athena', {
+    crop: 'crop',
+    width: 1500,
+    height: 3000,
+    gravity: 'north_west',
+  })
+);
+console.log(
+  cloudinary.url('fashion-athena', {
+    crop: 'crop',
+    width: 1500,
+    height: 3000,
+    gravity: 'north_east',
+  })
+);
+console.log(
+  cloudinary.url('fashion-athena', {
+    crop: 'crop',
+    width: 1500,
+    height: 3000,
+    gravity: 'south_west',
+  })
+);
+console.log(
+  cloudinary.url('fashion-athena', {
+    crop: 'crop',
+    width: 1500,
+    height: 3000,
+    gravity: 'south_east',
+  })
+);
+
+// crop all but the bag
+console.log(
+    cloudinary.url('fashion-athena', {
+      crop: 'thumb',
+      width: 400,
+      height: 400,
+      gravity: 'auto:bag_avoid',
+      sign_url: true,
+    })
+  );
+
+// crop other tagged items
 
 
 
 
-// crop (also try fill, lfill_pad, thumb)
-console.log(cloudinary.url('fashion-athena',{crop:"crop",gravity:"stockings",sign_url:true}))
-console.log(cloudinary.url('fashion-athena',{crop:"crop",width:400,gravity:"auto:bag",sign_url:true}))
-console.log(cloudinary.url('fashion-athena',{crop:"crop",width:400,height:400,gravity:"auto:bag_avoid",sign_url:true}))
 
+// all the tagged objects
+console.log(
+    cloudinary.url('fashion-athena', {
+      crop: 'crop',
+      gravity: 'stockings',
+      sign_url: true,
+    })
+  );
+console.log(
+    cloudinary.url('fashion-athena', {
+      crop: 'fill',
+      width: 400,
+      height: 400,
+      gravity: 'coat',
+      sign_url: true,
+    })
+  )
 
+  console.log(
+    cloudinary.url('fashion-athena', {
+        crop: 'fill',
+        width: 400,
+        height: 400,
+      gravity: 'dress',
+      sign_url: true,
+    })
+  )
 
-
+  console.log(
+    cloudinary.url('fashion-athena', {
+        crop: 'thumb',
+        width: 400,
+        height: 400,
+      gravity: 'footwear',
+      sign_url: true,
+    })
+  )
+  console.log(
+    cloudinary.url('fashion-athena', {
+        crop: 'thumb',
+        width: 400,
+        height: 400,
+      gravity: 'stockings',
+      sign_url: true,
+    })
+  )
