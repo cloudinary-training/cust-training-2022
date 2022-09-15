99059271340a177cf23ed8d3bb6e9fff72374b92
rebeccapeltz <rebecca.peltz@cloudinary.com>
2022-09-15 11:21:33 -0700

    show data extracted

--- ai/ocr-detection/upload.js
+++ ai/ocr-detection/upload.js
@@ -1,25 +1,33 @@
 require('dotenv').config();
 const cloudinary = require('cloudinary').v2;
 
-cloudinary.uploader.upload('./assets/black_car.jpg', {
-    public_id: 'black_car',
-    ocr: 'adv_ocr'
+cloudinary.uploader
+  .upload('./assets/black_car.jpg', {
+    public_id: 'ai/black_car_1',
+    ocr: 'adv_ocr',
   })
-  .then(uploadResult => {
-    console.log(JSON.stringify(uploadResult, null, 2))
-    console.log(cloudinary.url('black_car', {
-      transformation: [{
-          width: 1520,
-          height: 1440,
-          gravity: 'west',
-          x: 50,
-          crop: 'crop'
-        },
-        {
-          effect: 'pixelate_region:15',
-          gravity: 'ocr_text'
-        }
-      ]
-    }));
+  .then((uploadResult) => {
+    console.log(JSON.stringify(uploadResult, null, 2));
+    console.log(
+      'text detected:',
+      uploadResult.info.ocr.adv_ocr.data[0].textAnnotations[0].description
+    );
+    console.log(
+      cloudinary.url('ai/black_car', {
+        transformation: [
+          {
+            width: 1520,
+            height: 1440,
+            gravity: 'west',
+            x: 50,
+            crop: 'crop',
+          },
+          {
+            effect: 'pixelate_region:15',
+            gravity: 'ocr_text',
+          },
+        ],
+      })
+    );
   })
-  .catch(error => console.error(error));
+  .catch((error) => console.error(error));
