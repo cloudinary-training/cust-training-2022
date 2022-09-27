5fc481a68a9efdfeafd5408a95c56c52bb1032df
rebeccapeltz <rebeccapeltz@gmail.com>
2022-09-27 10:59:46 -0700

    update video tagging code/response' git push

--- ai/auto-tagging/video-upload-tag.js
+++ ai/auto-tagging/video-upload-tag.js
@@ -6,8 +6,10 @@ cloudinary.uploader
     public_id: 'ai/hat',
     resource_type: 'video',
     categorization: 'google_video_tagging',
-    auto_tagging: 0.7,
-    notification_url: "https://webhook.site/1d0a41ba-d2e7-4840-afab-84241eb78fc8"
+    auto_tagging: 0.5,
+    overwrite: true,
+    invalidate: true,
+    notification_url: "https://webhook.site/0057f756-8c9c-41d5-96e6-346f08e9cb59"
   })
   .then((result) => {
     // log google tagging result
